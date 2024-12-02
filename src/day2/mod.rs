f697be03aec5d0aff8119998bdb8dba99e0caa6f
jonathanGB <jonathan.guillotte.blouin@gmail.com>
2024-12-02 02:43:58 -0500

    Simplified day 2 solution.

--- src/day2/mod.rs
+++ src/day2/mod.rs
@@ -3,48 +3,13 @@ use crate::solver::Solver;
 pub struct SolverImpl {}
 
 impl SolverImpl {
-    fn is_safe_level(levels: &[Option<i32>]) -> bool {
-        // Either the 1st or the 2nd level must be present (both cannot be absent).
-        // Note that levels are only potentially ignored in part 2.
-        let (mut previous_level, start_index) = match levels[0] {
-            Some(level_0) => (level_0, 1),
-            None => match levels[1] {
-                Some(level_1) => (level_1, 2),
-                None => unreachable!(),
-            },
-        };
-        let mut report_ordering = None;
-        for i in start_index..levels.len() {
-            // Ignored values (i.e. None) are skipped.
-            let curr_level = match levels[i] {
-                Some(curr_level) => curr_level,
-                None => continue,
-            };
-
-            let curr_ordering = previous_level.cmp(&curr_level);
-            // Contiguous levels cannot be equal.
-            if curr_ordering.is_eq() {
-                return false;
-            }
-
-            match report_ordering {
-                None => report_ordering = Some(curr_ordering),
-                // Contiguous levels have to follow the same ordering throughout the report.
-                Some(report_ordering) if report_ordering != curr_ordering => return false,
-                // If we have a determined ordering and it matches the current pair ordering, nothing to do.
-                Some(_) => {}
-            };
-
-            let diff = (curr_level - previous_level).abs();
-            // Contiguous levels can at most have a difference of 3.
-            if diff > 3 {
-                return false;
-            }
-
-            previous_level = curr_level;
-        }
-
-        return true;
+    fn is_safe_level(levels: &[i32]) -> bool {
+        levels
+            .windows(2)
+            .all(|window| (1..=3).contains(&(window[1] - window[0])))
+            || levels
+                .windows(2)
+                .all(|window| (-3..=-1).contains(&(window[1] - window[0])))
     }
 }
 
@@ -55,7 +20,7 @@ impl Solver for SolverImpl {
         for line in file.lines() {
             let levels: Vec<_> = line
                 .split(' ')
-                .map(|level| Some(level.parse::<i32>().unwrap()))
+                .map(|level| level.parse::<i32>().unwrap())
                 .collect();
 
             if Self::is_safe_level(&levels) {
@@ -70,32 +35,20 @@ impl Solver for SolverImpl {
         let mut num_safe_reports = 0;
 
         for line in file.lines() {
-            let mut levels: Vec<_> = line
+            let levels: Vec<_> = line
                 .split(' ')
-                .map(|level| Some(level.parse::<i32>().unwrap()))
+                .map(|level| level.parse::<i32>().unwrap())
                 .collect();
 
-            if Self::is_safe_level(&levels) {
-                num_safe_reports += 1;
-            } else {
-                // The current report is unsafe. Try to ignore each level one after the other, until we
-                // find one combination that is deemed safe.
-
-                // This keeps track of the previous value that was ignored when checking a new report combination.
-                // This is important when we need to try a new report combination, so that the previous level
-                // that was ignored is replaced with its original value.
-                let mut previous_ignored_value = None;
-                for i in 0..levels.len() {
-                    if previous_ignored_value.is_some() {
-                        levels[i - 1] = previous_ignored_value;
-                    }
-
-                    previous_ignored_value = std::mem::take(&mut levels[i]);
-
-                    if Self::is_safe_level(&levels) {
-                        num_safe_reports += 1;
-                        break;
-                    }
+            for i in 0..levels.len() {
+                // Create a copy of the original `levels`, with the ith level removed.
+                // Note that already safe reports will still be safe if we remove the first level,
+                // hence why we do not need to first check whether a full report is safe before
+                // moving on to spliced report combinations.
+                let spliced_levels = [&levels[..i], &levels[i + 1..]].concat();
+                if Self::is_safe_level(&spliced_levels) {
+                    num_safe_reports += 1;
+                    break;
                 }
             }
         }
